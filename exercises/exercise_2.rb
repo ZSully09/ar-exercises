c157948be9843582a5be8c815d6c7b07a57ac013
ZSully09 <zsullivan93@gmail.com>
2019-12-10 17:52:01 +0000

    exercise 2 commit

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.first
+puts "Store1 ID: #{@store1.id}"
+@store2 = Store.second
+puts "Store2 ID: #{@store2.id}"
+
+@store1.update(name: 'Bamboozled')
+@store1.save
+puts @store1.name
